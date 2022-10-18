75a64452c1533c10d307727829d4feb1ed8c930f
PillageDev <pillageyt@gmail.com>
2022-10-18 18:33:21 -0400

    + files

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+
+function setRank() {
+    if (money > 100000);
+    document.getElementById("rank").innerHTML = rank;
+    if (money > 0);
+    document.getElementById("money").innerHTML = money;
+    
+
+}
+
+let money = 1;
+
+function addMoney() {
+    money += 1;
+    
+}
+
+console.log(addMoney);
